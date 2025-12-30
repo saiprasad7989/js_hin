1569308a0bc0c21e779d5b88ca798bb916464135
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2025-12-30 09:40:34 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version//
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability should be main priority //
+
+console.log("sai")
+
+
+let name = "saiprasad"
+let age = 23
+let isloggedIn = false
+let state;
+
+//number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>  standalone value 
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
