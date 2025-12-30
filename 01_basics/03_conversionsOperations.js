eb12f40421cb9700edc3762e586157c3dd51b577
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2025-12-30 12:12:21 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,29 @@
+let score = "sai"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedin = "sai"
+
+let booleanIsLoggedIn = Boolean(isLoggedin)
+//console.log(booleanIsLoggedIn);
+
+
+// 1=> true; 0 => false
+// "" => false 
+// "sai" true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
