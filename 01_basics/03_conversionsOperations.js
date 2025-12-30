64a0ef86693b0d5e1ef7c81998b3bcc7675af818
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2025-12-30 13:18:16 +0000

    operations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -24,6 +24,43 @@ let booleanIsLoggedIn = Boolean(isLoggedin)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************** operations **************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2/3);
+
+let str1 = "hello"
+let str2 = " sai"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2")
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// link to study prefix(++x) and postfix (x++)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
