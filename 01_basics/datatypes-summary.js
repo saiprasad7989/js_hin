e35d103c4103c42ecfaedb5f81a9ec426f0091ed
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2025-12-31 10:13:42 +0000

    stack and heap memory operations

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 //https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myCollegeName = "sreyascollege"
+
+const anothername = "myCollegeName"
+// anothername = "datascience"
+
+console.log(myCollegeName);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sai@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
