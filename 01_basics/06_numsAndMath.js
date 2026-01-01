8e240ab35d55e6888d50809c2eb2e6a8ee8d43e9
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-01-01 17:59:18 +0000

    Numbers

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,16 @@
+const score = 200
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(5));
+
+const otherNumber = 123.555
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 129550000
+console.log(hundreds.toLocaleString('en-In'));
+
+
