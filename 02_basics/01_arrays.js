673c0b5fef6ece01af9a5f138859371de08812d7
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-01-02 18:46:44 +0000

    Arrays, Array methods, Slice and Splice

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["spiderman", "ironman"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
+
+//Array methods
+
+// myArr.push(10) /// adding number
+// myArr.push(7)
+// myArr.pop()     /// removing last digit
+
+// myArr.unshift(9)  /// adding the number at first place 
+// myArr.shift()       /// shift acts like 'pop' but it removes first number
+
+// console.log(myArr.includes(3)) /// discribes boolian
+// console.log(myArr.indexOf(3))   /// digit placment in the object
+
+// const newArr = myArr.join() /// 'join' bind the arrays which are in brackets converts into string.
+// console.log(myArr);
+// console.log(newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log("B ", myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
