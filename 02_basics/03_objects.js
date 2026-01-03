c226bbed8fed21b2c91e36d20abb440dbe2620ce
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-01-03 17:31:58 +0000

    objects in depth

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,45 @@
+// singleton
+
+// objects literals
+// Object.create
+
+const mySym = Symbol("key1")    /// to declare symbol
+
+
+const JsUser = {
+    name : "sai prasad",
+    "full name" : "b t n sai prasad",
+    [mySym] : "mykey1",  ///symbol 
+    age : 23,
+    location : "hyd",
+    email : "saiprasa@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["monday", "saturday"]
+}
+
+//  console.log(JsUser.email)
+//  console.log(JsUser["email"])
+//  console.log(JsUser["full name"])
+//  console.log(JsUser[mySym]);
+ 
+
+ JsUser.email = "sai@chatgpt.com"
+//  Object.freeze(JsUser)
+ JsUser.email = "sai@microsoft.com"
+
+//  console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello js user");
+    
+}
+ JsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`);
+ }
+ 
+ console.log(JsUser.greeting());
+ console.log(JsUser.greetingTwo());
+ 
+
+ 
+ 
