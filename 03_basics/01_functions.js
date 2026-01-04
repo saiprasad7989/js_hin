dc14dedd1d654a2cdd67722233a8895417b8e2ca
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-01-04 19:32:28 +0000

    functions and parameters

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,37 @@
+ function sayMyName(){
+    console.log("s");
+    console.log("a");
+    console.log("i");
+ }
+
+//  sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+    
+// }
+
+function addTwoNumbers(number1, number2){
+    
+    // let result = number1 + number2
+    // return result
+    return number1 +number2
+    
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("result: ", result);
+
+
+function loginUsersMessage(username = "computer"){
+    if(!username){
+        console.log("please enter username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUsersMessage())
+
+
