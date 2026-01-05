3ea431c420d06f67f1a1db65c97f4eb0b6f7347c
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-01-05 18:54:16 +0000

    scope and mini hosting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,11 +6,54 @@ let a = 300
  if (true){
     let a = 10
     const b = 20
-    console.log("inner: ", a);
+   //  console.log("inner: ", a);
     
  }
- console.log(a);
+//  console.log(a);
 //  console.log(b);
 //  console.log(c);
  
+
+function one(){
+   const username = "sai"
+
+   function two(){
+      const website = "youtube"
+      console.log(username);
+      
+   }
+   // console.log(website);
+
+   // two()
+   
+}
  
+one()
+
+if(true) {
+   const username = "saiprasad "
+   if(username === "saiprasad "){
+      const website = "youtube"
+      // console.log(username + website);
+      
+   }
+   // console.log(website);
+   
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++ interesting +++++++++++++++++++++++
+
+function addone(num){
+   return num + 1
+}
+
+console.log(addone(5))
+
+
+addTwo(5)
+const addTwo = function(num){
+   return num + 2
+}
+
