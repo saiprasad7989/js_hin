db812a6751b387ab39bd12985dd6f822a49e6021
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-01-07 18:16:18 +0000

    this method works on current variables.

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,19 @@
+const user = {
+    username: "sai",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+        
+    }
+    
+}
+
+
+user.welcomeMessage()
+user.username = "amar"
+user.welcomeMessage()   
+
+// console.log(this);
