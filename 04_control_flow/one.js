0b05490c9e9a842699d4dcbe976210e11e4ccd67
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-14 11:05:40 +0000

    if

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,17 @@
 //if
+const userLoggedIn = true
+const temp = 41
 
-if (true){
-
+if (temp === 50){
+    console.log("less than 50");
 }
-if (false){
+if (temp === 50){
+    console.log("greater than 50");
     
 }
+if (temp <= 41){
+    console.log("less than or equal to 41");
+    
+}
+
+// < - less than, > - greaterthan, <= - lessthan or equal, >= Greaterthan or equal, == - eual, != - not equal, === = type.
