c7acc22aae3be65d2ac08b6db6c7c102b5002910
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-14 12:23:47 +0000

    else

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -9,9 +9,15 @@ if (temp === 50){
     console.log("greater than 50");
     
 }
-if (temp <= 41){
-    console.log("less than or equal to 41");
+if (temp === 41) {
+    console.log(" is less than or equalto 41");
+    
+} else {
+    console.log("nothing");
     
 }
+
+console.log("sreyas");
+
 
 // < - less than, > - greaterthan, <= - lessthan or equal, >= Greaterthan or equal, == - eual, != - not equal, === = type.
