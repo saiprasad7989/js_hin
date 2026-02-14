91d75af4da68dbe8ded86de124a638584d205ce2
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-14 18:56:13 +0000

    condtions will apply within the scope. not outside the scope

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,22 +2,32 @@
 const userLoggedIn = true
 const temp = 41
 
-if (temp === 50){
-    console.log("less than 50");
-}
-if (temp === 50){
-    console.log("greater than 50");
+// if (temp === 50){
+//     console.log("less than 50");
+// }
+// if (temp === 50){
+//     console.log("greater than 50");
     
-}
-if (temp === 41) {
-    console.log(" is less than or equalto 41");
+// }
+// if (temp === 41) {
+//     console.log(" is less than or equalto 41");
     
-} else {
-    console.log("nothing");
+// } else {
+//     console.log("nothing");
     
-}
+// }
 
-console.log("sreyas");
+// console.log("sreyas");
 
 
 // < - less than, > - greaterthan, <= - lessthan or equal, >= Greaterthan or equal, == - eual, != - not equal, === = type.
+
+const score = 200
+
+if (score > 100) {
+    const power = "fly"
+    console.log(`user power : ${power}`);
+    
+}
+
+// console.log(`user power : ${power}`);
