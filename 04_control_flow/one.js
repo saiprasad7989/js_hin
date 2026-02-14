efe99b60c028f099211cf6f5f8cea1928c242d4e
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-14 19:17:38 +0000

    && is "and"

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,6 @@
 //if
-const userLoggedIn = true
-const temp = 41
+// const userLoggedIn = true
+// const temp = 41
 
 // if (temp === 50){
 //     console.log("less than 50");
@@ -32,8 +32,28 @@ const temp = 41
 
 // console.log(`user power : ${power}`);
 
-const balance = 1000
+// const balance = 1000
 
-if (balance > 500) console.log("test"),
-console.log("test2");
+// // if (balance > 500) console.log("test"), console.log("test2");
 
+// if (balance < 500){
+//     console.log("less than 500");
+    
+// } else if (balance < 750){
+//     console.log("less than 750");
+    
+// } else if (balance < 900 ){
+//     console.log("less than 900");
+    
+// } else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("allow to buy the courses");
+    
+}
