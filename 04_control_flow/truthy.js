16cbe80d54b0fb0ca23a0664705b44fc676d1f5a
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-15 11:20:45 +0000

    Falsy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,15 @@
+const userEmail = [ ]
+
+if (userEmail){
+    console.log("got user email");
+    
+} else {
+    console.log("don't have user email");
+    
+}
+
+
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, ,NaN(not a Number)
