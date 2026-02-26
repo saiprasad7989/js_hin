8e4b28d76e8cc10e2a1fc7cda9bbcaba59154d82
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-26 14:10:54 +0000

    to stop the number

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,8 +1,48 @@
 // for loop
 
-for (let index = 0; index <= 10; index++) {
-    const element = index;
-    console.log(element);
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 7){
+        // console.log("7th number is best one");
+    }
+    // console.log(element);
     
     
+}
+
+
+// select the variable and press "command + D" to change more than one variable
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`outer loop value :${i}`);
+    
+    for (let j = 0; j < 10; j++) {
+        // console.log('inner loop value : ${j} and outer loop :${i}');
+        // console.log(i + '*' + j + "=" + i*j);
+        
+       
+        
+    }
+    
+}
+
+let myArray = ["spyderman", "ironman", "batman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+// break and conditions
+
+for (let index = 1; index < 40; index++) {
+    if (index == 5) {
+        console.log(`ditected 5`);
+        break;
+
+        
+    }
+    
+    console.log(`value of ${index}`);
+        
 }
