d4c1530d4fd0e659996edf4beee480b1a4a96afa
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-26 16:01:45 +0000

    do while

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,3 +1,4 @@
+//  while loop
 
 // let index = 0
 // while (index <= 10) {
@@ -7,11 +8,20 @@
     
 // }
 
-let myArray = ["hyd", "mumbai", "delhi"]
-let arr = 0
-while (arr < myArray.length) {
-    console.log(`value is ${myArray[arr]}`);
-    arr = arr + 1
+// let myArray = ["hyd", "mumbai", "delhi"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1
 
     
-}
+// }
+
+// do while
+
+let score = 1
+do {
+    console.log(`the score is ${score}`);
+    score++
+    
+} while (score <= 10);
