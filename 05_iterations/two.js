6bce1a389fa7e1386ca64a77a42004c50fae9680
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-26 15:21:03 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+
+    
+// }
+
+let myArray = ["hyd", "mumbai", "delhi"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+
+    
+}
