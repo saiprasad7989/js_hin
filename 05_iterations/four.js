1f4a014ce7f95e7b86bdb1b1b463b804158cb614
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-27 17:21:04 +0000

    key in object

---
+++ 05_iterations/four.js
@@ -0,0 +1,13 @@
+const myObject = {
+    js : 'javascript',
+    py : 'python',
+    rb : 'ruby',
+    Cpp  : 'c++'
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]} `);
+
+}
+
+    
