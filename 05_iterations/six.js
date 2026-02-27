0ec32600b945ebca9aa85f91d5b80675d4098d49
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-27 18:31:05 +0000

    or

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -3,4 +3,10 @@ const myNums = [1,2,3,4,5,6,7,8]
 const newNums = myNums.filter(num => {
     return num > 4
 })
+
+                // or
+
+                
+console.log(newNums);
+const newNums2 = myNums.filter((num) => num > 4)
 console.log(newNums);
