4848a7b0f3ad53be11616fa03968a17acbd1505d
Sai Prasad <123759102+saiprasad7989@users.noreply.github.com>
2026-02-27 18:00:22 +0000

    accessing the arrays in object

---
+++ 05_iterations/five.js
@@ -0,0 +1,48 @@
+const coding = ['js', 'python', 'typescript', 'css', 'react']
+
+// coding.forEach( function name (val) {
+//     // console.log(val);
+    
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item) {
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+    
+} )
+
+const myCoding = [
+    {
+        languageName : "java script",
+        languageFileName : "js",
+        
+    },
+    {
+        languageName : "python",
+        languageFileName : "py",
+
+    },
+    {
+        languageName : "java",
+        languageFileName : "java",
+
+    }
+]
+
+myCoding.forEach((item) =>{
+    {}
+    console.log(item.languageName);
+    
+
+})
